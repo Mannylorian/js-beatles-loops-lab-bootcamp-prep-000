c5d2f524a0d6cbce89b82f89267b25d7dba2ae98
Emmanuel Anderson <manu1526@gmail.com>
2018-05-03 13:27:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var musicianString;
-  var musicianInstrument[];
+  var musicianInstrument=[];
     for(var i=0; i<musicians.length; i++){
       musicianString= musicians[i] + ' plays ' + instruments[i];
       musicianInstrument[i]=musicianString;
