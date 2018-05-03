b1baea9667dd7577a410ef324aca0c0654e77eef
Emmanuel Anderson <manu1526@gmail.com>
2018-05-03 13:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const facts=[
 function johnLennonFacts(factsArray){
   var i=0;
   var newArray=[];
-  while (factsArray > i){
+  while (factsArray.length > i){
     newArray.push(`${factsArray[i]}` + "!!!");
     i++;
     }
