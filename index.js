f3a82d30bf8d227772ac860ce0a95ba3d7a067e2
Emmanuel Anderson <manu1526@gmail.com>
2018-05-03 13:44:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 
-const facts= [
+const facts=[
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
   ];
 function johnLennonFacts(factsArray){
-  var i =0;
+  var i=0;
   var newArray=[];
   while (factsArray > i){
     newArray.push(`${factsArray[i]}` + "!!!");
